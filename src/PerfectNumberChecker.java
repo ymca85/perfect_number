021025c6c77d6d1a27ff509f8324ebc70ecdb2a5
ymca85 <bhushan.ymca85@gmail.com>
2016-11-29 21:16:16 +0530

    refactor
    removed constant value used and updated method to correct the logic

--- src/PerfectNumberChecker.java
+++ src/PerfectNumberChecker.java
@@ -1,24 +1,28 @@
 
 public class PerfectNumberChecker {
 
-	private final int NUMBER_TWO = 2;
+	public static void main(String[] args) {
+
+		PerfectNumberChecker perfectNumberChecker = new PerfectNumberChecker();
+		System.out.println(perfectNumberChecker.isPerfectNumber(Integer.parseInt(args[0])));
+	}
 
 	public boolean isPerfectNumber(int numberToTest) {
-		if (isNumberValidToCheck(numberToTest)) {
+		if (!isNumberValidToCheck(numberToTest)) {
 			return false;
 		}
 		return (numberToTest == getSumOfFactorsOfNumber(numberToTest));
 	}
 
 	private boolean isNumberValidToCheck(int numberToTest) {
-		return numberToTest < NUMBER_TWO;
+		return numberToTest > 0;
 	}
 
 	public int getSumOfFactorsOfNumber(int numberToTest) {
 
-		int sumOfFactorsOfNumber = 1;
+		int sumOfFactorsOfNumber = 0;
 
-		for (int i = 2; i <= (numberToTest / 2); i++) {
+		for (int i = 1; i <= (numberToTest / 2); i++) {
 			if (numberToTest % i == 0) {
 				sumOfFactorsOfNumber = sumOfFactorsOfNumber + i;
 			}
