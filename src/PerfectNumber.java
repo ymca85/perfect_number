1e1fb3f0f710779813c8e76f45b309687fce0a8e
ymca85 <bhushan.ymca85@gmail.com>
2016-11-29 16:51:17 +0530

    first commit for perfect number

---
+++ src/PerfectNumber.java
@@ -0,0 +1,8 @@
+
+public class PerfectNumber {
+
+	public void isPerfectNumber() {
+		
+	}
+
+}
---
+++ test/PerfectNumberTests.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PerfectNumberTests {
+
+	@Test
+	public void testPerfectNumber() {
+		
+		PerfectNumber perfectNumber = new PerfectNumber();
+		perfectNumber.isPerfectNumber();
+		
+	}
+
+}
